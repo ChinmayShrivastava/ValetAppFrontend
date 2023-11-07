339894e79be6e3e9f6809c61b195a876286add11
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2023-11-07 17:09:51 +0530

    Update signupbox.tsx

--- src/components/signupbox.tsx
+++ src/components/signupbox.tsx
@@ -98,7 +98,9 @@ export default function SignupBox() {
                             <label htmlFor="confirmPassword" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
                             <input type="password" name="confirmPassword" id="confirmPassword" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={handleConfirmPasswordChange} required />
                         </div>
-                        <button type="submit" className={"w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"+auth.registerloading ? "pointer-events-none opacity-50" : ""} onClick={handleSubmit} >Sign up</button>
+                        <button type="submit" className={
+                            "w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" + 
+                            auth.registerloading ? " pointer-events-none opacity-50" : ""} onClick={handleSubmit} >Sign up</button>
                         <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                             Already have an account? <Link href="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Log in</Link>
                         </p>
