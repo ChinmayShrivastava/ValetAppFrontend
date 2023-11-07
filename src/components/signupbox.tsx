475e8f9e24488ef8186771ec753ddff798c18b47
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2023-11-07 13:12:54 +0530

    Update signupbox.tsx

--- src/components/signupbox.tsx
+++ src/components/signupbox.tsx
@@ -6,6 +6,7 @@ import { AppDispatch } from '@/redux/store';
 import { useAppSelector } from '@/redux/store';
 import { checkAuthAPI , registerAPI } from '@/functions/auth';
 import { useState , useEffect } from 'react';
+import Link from 'next/link';
 
 export default function SignupBox() {
 
@@ -98,11 +99,8 @@ export default function SignupBox() {
                             <input type="password" name="confirmPassword" id="confirmPassword" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={handleConfirmPasswordChange} required />
                         </div>
                         <button type="submit" className={"w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"+auth.registerloading ? "pointer-events-none opacity-50" : ""} onClick={handleSubmit} >Sign in</button>
-                        <p className={
-                            "text-sm font-light text-gray-500 dark:text-gray-400" +
-                            auth.registerloading ? "pointer-events-none opacity-50" : ""
-                        }>
-                            Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                        <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                            Already have an account? <Link href="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Log in</Link>
                         </p>
                     </form>
                 </div>
