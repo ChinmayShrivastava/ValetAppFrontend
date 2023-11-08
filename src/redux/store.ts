f1ba8381712f8d20769b0c2f6cf93aaebb412ffe
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2023-11-08 22:05:58 +0530

    --- redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,7 +3,6 @@ import noteReducer from './features/note-slice';
 import documentsReducer from './features/documents-slice';
 import notesReducer from './features/notes-slice';
 import authReducer from './features/auth-slice';
-import fileReducer from './features/file-upload-slice';
 import { TypedUseSelectorHook, useSelector } from 'react-redux';
 
 export const store = configureStore({
@@ -13,7 +12,6 @@ export const store = configureStore({
         notesReducer,
         documentsReducer,
         authReducer,
-        fileReducer,
     }
 })
 
